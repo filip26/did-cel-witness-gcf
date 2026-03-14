7da902c10f0baf0f97d05e4b470236c9868ed8db
Filip <filip26@gmail.com>
2026-03-14 01:19:48 +0100

    Add 'all' parameter to purge script

--- bin/purge-revisions.sh
+++ bin/purge-revisions.sh
@@ -4,24 +4,46 @@ FUNCTION_ID=$1
 
 if [ -z "$FUNCTION_ID" ]; then
   echo "Error: No function id provided."
-  echo "Usage: purge-revisions.sh <function-id>"
+  echo "Usage: purge-revisions.sh <function-id | all>"
   exit 1
 fi
 
 CONFIG_FILE="functions.json"
 
-REGION=$(jq -r --arg ID "$FUNCTION_ID" '.[] | select(.id == $ID) | .region' "$CONFIG_FILE")
-
-if [ "$REGION" == "null" ]; then
-  echo "Error: Configuration for $FUNCTION_ID not found."
-  exit 1
+if [ "$FUNCTION_ID" == "all" ]; then
+  FUNCTIONS=$(jq -r --arg ID "$FUNCTION_ID" '.[] | .id' "$CONFIG_FILE")
+else
+  FUNCTIONS="$FUNCTION_ID"
 fi
 
-REVISIONS=$(gcloud run revisions list \
-  --service=$FUNCTION_ID \
-  --region=$REGION \
-  --sort-by="~metadata.creationTimestamp" \
-  --format="value(metadata.name)" | tail -n +3)
+
+REVISIONS=""
+
+for FNC in $FUNCTIONS; do
+
+  REGION=$(jq -r --arg ID "$FNC" '.[] | select(.id == $ID) | .region' "$CONFIG_FILE")
+  
+  if [ -z "$REGION" ]; then
+    echo "Error: Configuration region for $FNC not found."
+    exit 1
+  fi
+  
+  printf "%s\t%s\t" $FNC $REGION
+
+  NR=$(gcloud run revisions list \
+    --service=$FNC \
+    --region=$REGION \
+    --sort-by="~metadata.creationTimestamp" \
+    --format="value(metadata.name)" | tail -n +3)
+
+  if [ "$NR" != "" ]; then
+    REVISIONS=${REVISIONS[@]}$'\n'${NR[@]}
+    printf "%s\n" $(wc -l <<< $NR)
+  else 
+    printf "OK\n"    
+  fi
+
+done
     
 for REV in $REVISIONS; do 
   gcloud run revisions delete $REV --region=$REGION --quiet
